52eb6ce2525ced446abe5b3de957d288154103d6
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-01-25 15:55:32 +0200

    setting up themeContect, background and index files

---
+++ src/pages/background.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Background = ({childern})=>{
+   return(
+    <div className="bg-white dark:bg-black translation-all">
+
+    </div>
+   )
+}
+export default Background;
